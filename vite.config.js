41ecedbdd2631fa663f109ad7cb906b40410418d
samruddhik07 <samruddhikarale877@gmail.com>
2026-02-22 17:13:20 +0530

    fixing vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()], // This plugin handles the React auto-importing for you
-})
+  plugins: [react()],
+});
